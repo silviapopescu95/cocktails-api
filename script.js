7a82596c9cccf71b101fb07b466ab955a0bc6cc6
Silvia Popescu <amazingaivlis@gmail.com>
2020-08-18 13:19:28 -0700

    created card to display cocktail name. Created logic to append html and db content on page.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
     // Accepts user input from on user's click
     $("#cocktail-search").on("click", function () {
-        var cocktailValue = $("#cocktail-value").val();
-
+        // let cocktailValue = $("#cocktail-value").val();
+        let cocktailValue = "margarita"
         // clears input field
         $("#cocktail-search").val("");
 
@@ -17,13 +17,22 @@ $(document).ready(function () {
             url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktailValue,
             dataType: "json",
             success: function (data) {
-                console.log(data);
-                console.log(data.drinks[0].strDrink)
-                console.log(data.drinks[0].strGlass)
+                // console.log(data);
+                // console.log(data.drinks[0].strDrink)
+                // console.log(data.drinks[0].strGlass)
 
                 // clear any old content
                 $("#cocktail").empty();
 
+                // creates the string from the drink name db detail
+                let drinkName = $("<h3>").addClass("card-title").text(data.drinks[0].strDrink);
+                console.log(drinkName)
+                let card = $("<div>").addClass("card");
+                let cardBody = $("<div>").addClass("card-body");
+
+                cardBody.append(drinkName);
+                card.append(cardBody);
+                $("#cocktail").append(card);
 
             }
         })
