e4db6086a493246dd145c706ac410d71ea0b33fc
Silvia Popescu <amazingaivlis@gmail.com>
2020-08-18 13:07:29 -0700

    Did basic HTML setup, created a working AJAX call to the cocktail db

--- script.js
+++ script.js
@@ -6,8 +6,27 @@ $(document).ready(function () {
         // clears input field
         $("#cocktail-search").val("");
 
-        searchCocktail(searchValue);
+        searchCocktail(cocktailValue);
     });
+    // function to create a list here
 
+    // AJAX call
+    function searchCocktail(cocktailValue) {
+        $.ajax({
+            type: "GET",
+            url: "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + cocktailValue,
+            dataType: "json",
+            success: function (data) {
+                console.log(data);
+                console.log(data.drinks[0].strDrink)
+                console.log(data.drinks[0].strGlass)
+
+                // clear any old content
+                $("#cocktail").empty();
+
+
+            }
+        })
+    };
 
 });
